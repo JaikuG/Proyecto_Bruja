00ff3eaafb222f53a66d2bdfdfbe35024f852848
JaikuG <jaimefesd@gmail.com>
2023-04-19 17:39:05 +0200

    MapaGuay
    mapaguay

---
+++ Source/ProyectoBruja/MyActor7.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor7.h"
+
+// Sets default values
+AMyActor7::AMyActor7()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor7::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor7::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
